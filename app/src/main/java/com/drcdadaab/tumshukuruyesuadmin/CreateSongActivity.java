badf867849d80aabafb908aeee6cd7f0793b2a1d
dadaabcamps <dadaabcamps@gmail.com>
2017-09-26 12:47:54 +0100

    Fixed Language selection issue

--- app/src/main/java/com/drcdadaab/tumshukuruyesuadmin/CreateSongActivity.java
+++ app/src/main/java/com/drcdadaab/tumshukuruyesuadmin/CreateSongActivity.java
@@ -23,6 +23,7 @@ import com.google.firebase.database.FirebaseDatabase;
 
 public class CreateSongActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
     EditText editTextTitle;
+    Spinner spinnerCreateSong;
     EditText editTextHymnNumber;
     EditText editTextContent;
     String title, hymnnumber, content, songId, language;
@@ -35,29 +36,40 @@ public class CreateSongActivity extends AppCompatActivity implements AdapterView
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_song);
 
-        Spinner spinnerCreateSong = (Spinner) findViewById(R.id.spinnerCreateSong);
-// Create an ArrayAdapter using the string array and a default spinner layout
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
-                R.array.song_language, android.R.layout.simple_spinner_item);
-// Specify the layout to use when the list of choices appears
+        spinnerCreateSong = (Spinner) findViewById(R.id.spinnerCreateSong);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.song_language, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-// Apply the adapter to the spinner
         spinnerCreateSong.setAdapter(adapter);
+
+        spinnerCreateSong.setOnItemSelectedListener(this);
+
         db = FirebaseDatabase.getInstance();
         dbRef = db.getReference("Songs");
-
 
         editTextHymnNumber = (EditText) findViewById(R.id.editTextHymnNumber);
         editTextContent = (EditText) findViewById(R.id.editTextContent);
         editTextTitle = (EditText) findViewById(R.id.editTextTitle);
     }
 
-    public void openViewSongActivity (String title, String hymnnumber, String content){
+    public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
+        language = parent.getItemAtPosition(pos).toString();
+        if(language.equals("")){
+            Toast.makeText(CreateSongActivity.this, "Fill in all the fields", Toast.LENGTH_SHORT).show();
+            return;
+        }
+    }
+
+    public void onNothingSelected(AdapterView<?> parent) {
+        // Another interface callback
+    }
+
+    public void openViewSongActivity (String title, String hymnnumber, String content, String language){
         Intent intent = new Intent(CreateSongActivity.this,ViewSongActivity.class);
         intent.putExtra("idKey", songId);
         intent.putExtra("titleKey", title);
         intent.putExtra("HymnNumberKey", hymnnumber);
         intent.putExtra("ContentKey", content);
+        intent.putExtra("languageKey", language);
         startActivity(intent);
         finish();
     }
@@ -76,22 +88,9 @@ public class CreateSongActivity extends AppCompatActivity implements AdapterView
         dbRef.child(songId).setValue(song);
 
         Toast.makeText(CreateSongActivity.this, "Song Created", Toast.LENGTH_SHORT).show();
-        openViewSongActivity(title, hymnnumber, content );
+        openViewSongActivity(title, hymnnumber, content, language);
      }
 
-    public void onItemSelected(AdapterView<?> parent, View view,
-                               int pos, long id) {
-        // An item was selected. You can retrieve the selected item using
-        language = parent.getItemAtPosition(pos).toString();
 
-        if(language.equals("")){
-            Toast.makeText(CreateSongActivity.this, "Fill in all the fields", Toast.LENGTH_SHORT).show();
-            return;
-        }
-    }
-
-    public void onNothingSelected(AdapterView<?> parent) {
-        // Another interface callback
-    }
 
 }
